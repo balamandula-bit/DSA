4f95a5661cb19966988df0a22e902c20f35846a8
balamandula-bit <balamandula@gmail.com>
2026-08-11 19:01:02 +0530

    Time: 11 ms (82.49%) | Memory: 22.3 MB (42.08%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        n = len(strs)
+        group = {}
+
+        for i in range(n):
+            temp = sorted(strs[i])
+            temp = "".join(temp)
+            group.setdefault(temp, []).append(strs[i])
+        
+         
+        return list(group.values())
+
+        
